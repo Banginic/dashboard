6e4012e0409de5692de2b8df57347047ff2735c6
Banginic <borisndoh@gmail.com>
2025-09-04 11:27:48 +0100

    Add products page

---
+++ app/(dashboard)/dashboard/(secure)/products/[productId]/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { Title, Back, ErrorFetching, NoData } from "@/components/adminComponents/index";
+import ProductDetails from "@/components/adminComponents/ProductDetails";
+import ProductDetailsSkeleton from "@/components/adminComponents/skeletons/ProductDetailsSkeleton";
+import { useFetch } from "@/hooks/useFetch";
+import { ProductTypes } from "@/models/types";
+import { useQuery } from "@tanstack/react-query";
+import React, { use } from "react";
+
+function ProductDetail({ params}: { params: Promise<{ productId: string}>}) {
+  const { productId } = use(params)
+  const fetchDetails = {
+    endpoint: `/api/products/list-single-product?product_id=${productId}`,
+    method: 'GET',
+    title: 'Product details'
+  }
+  const { data, isPending, refetch, isError } = useQuery({
+    queryKey : [`kitchen-products-${productId}`],
+    queryFn: () => useFetch<ProductTypes>(fetchDetails)
+  })
+ console.log(data)
+  return (
+    <section className="w-[95%] mx-auto py-8 relative">
+      <div className="absolute top-10">
+        <Back link="/kitchen/products" />
+      </div>
+      <Title text1="Product" text2="Details" />
+       <div className="mt-12">
+       {
+         isPending ? (
+          <ProductDetailsSkeleton />
+         ) : isError || !data?.data || !data ? (
+          <ErrorFetching message="Product Details" retry={() => refetch}/>
+         ) : data.data.length === 0 && data?.message ? (
+          <NoData message={ data.message} />
+         ) : (
+          <ProductDetails product ={ data.data[0]} />
+         )
+       }
+
+       </div>
+    </section>
+  );
+}
+
+export default ProductDetail;
---
+++ app/(dashboard)/dashboard/(secure)/products/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Title from "@/components/adminComponents/Title";
+import { PlusCircle } from "lucide-react";
+import React, { useState } from "react";
+import { useFetch } from "@/hooks/useFetch";
+import {
+  ErrorFetching,
+  NoData,
+  ProductCard,
+} from "@/components/adminComponents/index";
+import { useQuery } from "@tanstack/react-query";
+import TestimonialForm from "@/components/adminComponents/TestimonialForm";
+import { ProductTypes } from "@/models/types";
+import ProductCardSkeleton from "@/components/adminComponents/skeletons/ProductCardSkeleton";
+import ProductForm from "@/components/adminComponents/ProductForm";
+
+function products() {
+  const [showProductForm, setProductForm] = useState(false);
+  const fetchDetails = {
+    endpoint: "/api/products/list-all-products?limit=20",
+    method: "GET",
+    title: "products",
+  };
+  const { data, isPending, refetch } = useQuery({
+    queryKey: ["admin-products"],
+    queryFn: () => useFetch<ProductTypes>(fetchDetails),
+  });
+
+  function openPharmacyForm() {
+    setProductForm(true);
+  }
+  return (
+    <div className="py-8 relative overflow-y-auto">
+      <Title text1="Products" />
+
+      <div className="w-[95%] max-w-3xl mx-auto">
+        <button
+          onClick={openPharmacyForm}
+          className="py-2 px-4 rounded bg-indigo-800 mt-12 cursor-pointer hover:opacity-80 text-neutral-100 text-sm flex items-center gap-2"
+        >
+          <PlusCircle size={18} />
+          <span>Add Product</span>
+        </button>
+
+        <section className="mt-12 ">
+          {isPending ? (
+            <ProductCardSkeleton />
+          ) : !data?.success || !data ? (
+            <ErrorFetching message={"products"} retry={refetch} />
+          ) : data?.data.length === 0 && data.message ? (
+            <NoData message={data.message} />
+          ) : (
+            <div className="flex flex-col gap-2 lg:gap-4 w-[95%] max-w-3xl mx-auto">
+              {data?.data.map((item) => (
+                <ProductCard key={item.id} product={item} />
+              ))}
+            </div>
+          )}
+        </section>
+      </div>
+
+      <>
+        {showProductForm && (
+          <div className="fixed inset-0 bg-black/80 h-screen border grid place-items-center ovrerflow--scroll">
+            <ProductForm closeForm={setProductForm} />
+          </div>
+        )}
+      </>
+    </div>
+  );
+}
+
+export default products;
