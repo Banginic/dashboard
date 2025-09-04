640ed5ee0337291950c8f843373098682741a2b0
Banginic <borisndoh@gmail.com>
2025-09-04 11:24:08 +0100

    Add news page

---
+++ app/(dashboard)/dashboard/(secure)/news/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import Title from "@/components/adminComponents/Title";
+import { PlusCircle } from "lucide-react";
+import React, { useState } from "react";
+import { useFetch } from "@/hooks/useFetch";
+import { NewsTypes } from "@/models/types";
+import {
+  ErrorFetching,
+  NewsDetails,
+  NoData,
+} from "@/components/adminComponents/index";
+import { useQuery } from "@tanstack/react-query";
+import NewsForm from "@/components/adminComponents/NewsForm";
+import NewsDetailsSkeleton from "@/components/adminComponents/skeletons/NewsDetailsSkeleton";
+
+function News() {
+  const [showNewsForm, setNewsForm] = useState(false);
+  const fetchDetails = {
+    endpoint: "/api/news/list-all-news?limit=15",
+    method: "GET",
+    title: "news",
+  };
+  const { data, isPending, refetch } = useQuery({
+    queryKey: ["kitchen-news"],
+    queryFn: () => useFetch<NewsTypes>(fetchDetails),
+  });
+  function toggleNewsForm() {
+    setNewsForm(true);
+  }
+  return (
+    <div className="py-8 relative">
+      <Title text1="News" />
+
+      <div className=" w-[95%] max-w-3xl mx-auto">
+        <button
+          onClick={toggleNewsForm}
+          className="py-2 px-4 rounded bg-indigo-800 mt-12 cursor-pointer hover:opacity-80 text-neutral-100 text-sm flex items-center gap-2"
+        >
+          <PlusCircle size={18} />
+          <span>Create News</span>
+        </button>
+        <section className="mt-12 ">
+          {isPending ? (
+            <NewsDetailsSkeleton />
+          ) : !data?.success ? (
+            <ErrorFetching message={"News"} retry={refetch} />
+          ) : data?.data.length === 0 && data?.message ? (
+            <NoData message={data.message} />
+          ) : (
+            <div className="flex flex-col gap-2 lg:gap-4 w-[95%] max-w-3xl mx-auto">
+              {data?.data.map((item) => (
+                <NewsDetails key={item.id} news={item} />
+              ))}
+            </div>
+          )}
+        </section>
+      </div>
+
+      <>
+        {showNewsForm && (
+          <div className="fixed inset-0 bg-black/80 grid place-items-center">
+            <NewsForm setNewsForm={setNewsForm} />
+          </div>
+        )}
+      </>
+    </div>
+  );
+}
+
+export default News;
