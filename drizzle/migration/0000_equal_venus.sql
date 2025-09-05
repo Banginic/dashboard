39201d2f883220dcac6ca019adfd25760a91e779
Banginic <borisndoh@gmail.com>
2025-09-05 13:08:56 +0100

    Add versioning v1

---
+++ drizzle/migration/0000_equal_venus.sql
@@ -0,0 +1,121 @@
+CREATE TABLE "contact_details" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"whatsApp" varchar(255) DEFAULT '' NOT NULL,
+	"email" varchar(255) DEFAULT '' NOT NULL,
+	"phone" varchar(255) DEFAULT '' NOT NULL,
+	"facebook" varchar(255) DEFAULT '' NOT NULL,
+	"instagram" varchar(255) DEFAULT '' NOT NULL,
+	"tiktok" varchar(255) DEFAULT '' NOT NULL,
+	"youTube" varchar(255) DEFAULT '' NOT NULL,
+	"twitter" varchar(255) DEFAULT '' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "project_info" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"project_name" varchar(255) DEFAULT '' NOT NULL,
+	"tag_line" varchar(255) DEFAULT '' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "employees" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"phone" varchar(255) NOT NULL,
+	"position" varchar(255) NOT NULL,
+	"qualification" varchar(255) NOT NULL,
+	"photo" varchar(255) NOT NULL,
+	"bio" varchar(500) NOT NULL,
+	"isActive" boolean DEFAULT true NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "location" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"country" varchar(255) DEFAULT '' NOT NULL,
+	"state" varchar(255) DEFAULT '' NOT NULL,
+	"city" varchar(255) DEFAULT '' NOT NULL,
+	"address" varchar(255) DEFAULT '' NOT NULL,
+	"currency" varchar(255) DEFAULT 'USD' NOT NULL,
+	"language" varchar(255) DEFAULT 'English (United States)' NOT NULL,
+	"map_pin" jsonb DEFAULT '{"lat":"","lgn":""}'::jsonb NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "messages" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"phone" varchar(255) NOT NULL,
+	"subject" varchar(255) NOT NULL,
+	"message" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "messages_email_unique" UNIQUE("email"),
+	CONSTRAINT "messages_message_unique" UNIQUE("message")
+);
+--> statement-breakpoint
+CREATE TABLE "news" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"subject" varchar(255) NOT NULL,
+	"body" varchar(500) NOT NULL,
+	"isActive" boolean DEFAULT true NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "orders" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"userId" uuid,
+	"location" varchar(255) NOT NULL,
+	"status" varchar(255) NOT NULL,
+	"phone" varchar(255) NOT NULL,
+	"orderDetails" jsonb NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "products" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"category" varchar(255) NOT NULL,
+	"sub_category" varchar(255) NOT NULL,
+	"description" varchar(500) NOT NULL,
+	"alergies" varchar(500) NOT NULL,
+	"rating" integer DEFAULT 4 NOT NULL,
+	"isInStock" boolean DEFAULT true NOT NULL,
+	"price" integer DEFAULT 0 NOT NULL,
+	"photos" jsonb NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "products_name_unique" UNIQUE("name")
+);
+--> statement-breakpoint
+CREATE TABLE "testimonies" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"project" varchar(255) NOT NULL,
+	"rating" integer DEFAULT 4.5 NOT NULL,
+	"photo" varchar(255) NOT NULL,
+	"message" varchar(250) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"phone" varchar(255) NOT NULL,
+	"role" varchar(255) DEFAULT 'client' NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_userId_users_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -14,7 +14,7 @@ export async function useFetch<T>( props : PropsTypes ): Promise<T | ReturnType>
     const {endpoint, method, title } = props
   try {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
-    const response = await fetch(`${baseUrl}/${title}${endpoint}`, {
+    const response = await fetch(`${baseUrl}/api/v1${endpoint}`, {
       method: method,
       headers: { "Content-Type": "application/json" },
     });
